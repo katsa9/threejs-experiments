73d2e2323a297cd4b7fab32802ef2fab4a2afa89
katsa9 <kathleen.brand2@gmail.com>
2022-06-09 16:52:07 +1000

    add more cubes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import * as THREE from 'three';
 
+function createCube(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.x = x;
+    return cube;
+}
+
 function main() {
   const canvas = document.querySelector('#c');
   const renderer = new THREE.WebGLRenderer({canvas});
@@ -18,13 +25,38 @@ function main() {
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-  const material = new THREE.MeshBasicMaterial({color: 0x44aa88});  // greenish blue
+  const material = new THREE.MeshPhongMaterial({color: 0x44aa88});  // greenish blue
+
+  //add a light
+  const color = 0xFFFFFF;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  light.position.set(-1, 2, 4);
+  scene.add(light);
 
   const cube = new THREE.Mesh(geometry, material);
+  const cube2 = createCube(geometry, 0x8844aa, -2)
+  const cube3 = createCube(geometry, 0x32b5ed,  2)
+  let elements = [cube, cube2, cube3]
   scene.add(cube);
+  scene.add(cube2)
+  scene.add(cube3)
 
-  renderer.render(scene, camera);
-  console.log("hello 5")
+
+  function render(time) {
+    time *= 0.001;  // convert time to seconds
+    elements.forEach((e) => {
+        e.rotation.x = time;
+        e.rotation.y = time;
+    })
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
 }
+
+
 
 main();
